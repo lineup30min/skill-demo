0cf15369bcdb62c243761968b88dbd2a6012bb0a
Sammy <xshe@ucsd.edu>
2022-05-01 17:45:50 -0700

    Added a test file

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,5 @@
 public class SkillDemo {
-// Let us take this to the VScode
+    public static int subtract (int a, int b) {
+        return a-b;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test 
+    public void subtraction() {
+		// A test that fails initially
+		assertEquals(3, SkillDemo.subtract(4,2));
+	}
+}
